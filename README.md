365daa2ccaf3bf859f40652abe9997aff0df82dd
Remco Troost <remcotroost@outlook.com>
2017-07-31 14:37:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ng2-start-systemjs
+
+A starting template for my Angular2 applications using SystemJS.
+
+I also have a starting template that uses Webpack instead.
---
+++ app/magic.ts
@@ -0,0 +1,10 @@
+class Greeter {
+
+  saySomething(message: string = 'whats up') {
+    return message + ' something';
+  }
+  
+}
+
+var greeter = new Greeter();
+greeter.saySomething('hello');
